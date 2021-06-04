40cf5d9ae64c34b9cab4dea2b6404a238e7ae0d9
Alessa <ernesto@Ernestos-MacBook-Pro.local>
2021-06-03 19:51:50 -0500

    lab-09 buddy

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,44 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Text,
     View,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 export const Row = ({ index, title, author }) => {
+    const [info, showInfo] = useState(false);
     return (
-        <View
-            style={[styles.row, {
-                backgroundColor: index % 2 ? '#CBC3E3' : 'white',
-            }]}>
-            <View style={styles.edges}>
-                <Text>{index}</Text>
+        <View>
+            <View
+                style={[styles.row, {
+                    backgroundColor: index % 2 ? '#CBC3E3' : 'white',
+                }]}>
+                <View style={styles.edges}>
+                    <Text>{index}</Text>
+                </View>
+                <View style={styles.titleBook}>
+                    <Text>{title}</Text>
+                    <Text style={styles.author}>{author}</Text>
+                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        showInfo(v => !v);
+                    }}
+                >
+                    <View style={styles.edges}>
+                        <Text>Info</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
-            <View style={styles.titleBook}>
-                <Text>{title}</Text>
-                <Text style={styles.author}>{author}</Text>
-            </View>
-            <View style={styles.edges}>
-                <Text>Info</Text>
-            </View>
+
+            {info && (
+                <View>
+                    <Text>Book Info</Text>
+                </View>)
+            }
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     row: {
-      flexDirection: 'row'
+        flexDirection: 'row'
     },
     edges: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: 50,
     },
     titleBook: {
-      flex: 8,
-      flexDirection: 'column'
+        flex: 8,
+        flexDirection: 'column'
     },
     author: {
-      color: 'gray'
+        color: 'gray'
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 2,
     }
-  });
+});
