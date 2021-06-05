2e1807b7fb9d52a0386ea3e5b954ff84b2620370
Alessa <ernesto@Ernestos-MacBook-Pro.local>
2021-06-05 12:20:41 -0500

    tab navigator

--- App.js
+++ App.js
@@ -9,31 +9,68 @@
 import React from 'react';
 import { BookList } from 'components/BookList';
 import { BookInfo } from 'components/BookInfo';
+import { About } from 'components/About';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 
 const Stack = createStackNavigator();
 
+const Tab = createBottomTabNavigator();
+
+
+const AppNavigator = () => {
+  return (<Stack.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+      title: 'Book Info',
+      headerStyle: {
+        backgroundColor: 'black'
+      },
+      headerTintColor: 'white',
+      headerTitleColor: {
+        color: 'white'
+      }
+    }}>
+    <Stack.Screen name="Home" component={BookList} />
+    <Stack.Screen name="Info" component={BookInfo} />
+  </Stack.Navigator>);
+}
+
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          title: 'Book Info',
-          headerStyle: {
-            backgroundColor: 'black'
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+
+            /* const iconName = {
+              'Home': 'home',
+              'About': 'info-circle',
+            }[route.name]; */
+
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'About') {
+              iconName = 'info-circle';
+            }
+
+            return <Icon name={iconName} size={23} color={color} />;
           },
-          headerTintColor: 'white',
-          headerTitleColor: {
-            color: 'white'
-          }
+        })}
+        tabBarOptions={{
+          activeBackgroundColor: 'lightgray',
+          activeTintColor: 'black',
+          inactiveTintColor: 'lightgray',
         }}>
-        <Stack.Screen name="Home" component={BookList} />
-        <Stack.Screen name="Info" component={BookInfo} />
-      </Stack.Navigator>
+        <Tab.Screen name="Home" component={AppNavigator} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator>
     </NavigationContainer>);
 };
 
